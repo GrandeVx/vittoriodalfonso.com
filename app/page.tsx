77d74e1925b2f6d8dae7ccfec7bccdd6e49f3d12
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-02-09 17:35:20 +0100

    Refactor page layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import TopBar from "@/layouts/TopBar";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between first-line:text-foreground xl:flex-row">
-      <section className="h-[7%] w-[92%] md:w-[90%] lg:w-[95%] xl:h-full xl:w-[50%] ">
+    <main className="flex h-screen flex-col gap-16 md:gap-0 items-center first-line:text-foreground xl:flex-row">
+
+      <section className="md:h-[4%] w-[92%] md:w-[90%] lg:w-[95%] xl:h-full xl:w-[50%]">
         <TopBar />
       </section>
+
       <main className="flex h-full w-[92%] flex-col gap-3 pt-4 selection:bg-orange-400/40 md:w-[90%] md:pr-[15%] md:pt-8 lg:pl-[23%] lg:pr-[15%] xl:px-[12%]">
         <p className=" text-pretty text-start font-sans text-sm">
           At the intersection of brand awareness, technical understanding, and
